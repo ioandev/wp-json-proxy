5d674d98753636c7b5a9b8ece476ff746168f2c3
Ioan Biticu <ioanb7code@gmail.com>
2020-10-03 13:56:10 +0100

    bug: popular posts showing hashtags in the title

--- src/popularPostsConvertor.js
+++ src/popularPostsConvertor.js
@@ -1,11 +1,21 @@
 const htmlToText = require('html-to-text');
 
 function popularPostsConvertor(data) {
-    return data.map(post => ({
-        title: htmlToText.fromString(post.title.rendered),
-        slug: post.slug,
-        pageviews: post.pageviews
-    }))
+    return data.map(post => {
+        
+        let title = htmlToText.fromString(post.title.rendered);
+
+        if (title.indexOf("#") != 0) {
+            let hashtags = title.split("#")
+            title = hashtags[0].trim()
+        }
+        
+        return {
+            title,
+            slug: post.slug,
+            pageviews: post.pageviews
+        }
+    })
 }
 
 export default popularPostsConvertor;
