c45edd323e033ba283e8fa60ac07ded2ee16889c
Ioan Biticu <ioanb7code@gmail.com>
2020-10-31 22:51:52 +0000

    fixed slugify

--- src/helpers/slugify.js
+++ src/helpers/slugify.js
@@ -1,3 +1,3 @@
 export default function (text) {
-    return text.toLowerCase().replace(/ /g,'-').replace(/[-]+/g, '-').replace(/[^\w-]+/g,'').replace(/^\d*[\s.-_]\-/g, '')
+    return text.toLowerCase().replace('.', '-').replace(/ /g,'-').replace(/[\-]+/g, '-').replace(/[^\w\-]+/g,'').replace(/^\d*[\s.-_]\-/g, '')
 }
